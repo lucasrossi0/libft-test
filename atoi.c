9dae5f4bec8fa5c39e44783b4b0b1d0afa6d27d9
Lucas Rossi <lucasrossi502@gmail.com>
2024-05-06 18:29:24 +0200

    Trying the atoi function

---
+++ atoi.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_atoi(const char *str) {
+  int i;
+  int pos;
+  int res;
+
+  i = 0;
+  pos = 1;
+  res = 0;
+  if (str[i] == '+' || str[i] == '-') {
+    if (str[i] == '-')
+      pos *= -1;
+    i++;
+  }
+  while (str[i] >= '0' && str[i] <= '9') {
+    res = res * 10 + (str[i] - '0');
+    i++;
+  }
+  return (res);
+}
+
+int main(void) {
+  const char *str = "-53";
+  int test = ft_atoi(str);
+  // printf("Testando %d", test);
+  int p1 = test / 10;
+  int p2 = test % 10;
+  // printf("Testando P1 %d", p1);
+  // printf("Testando P2 %d", p2);
+  //  write(1, &test, 5);
+  p1 += '0';
+  p2 += '0';
+  write(1, &p1, 1);
+  write(1, &p2, 1);
+  return (0);
+}
