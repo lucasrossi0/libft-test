c5eeac54b01d7778f90eec37152e9549cef5ec33
lucasrossi0 <lucasrossi502@gmail.com>
2024-05-22 18:59:25 +0200

    Trying to do memcmp function

--- memcmp.c
+++ memcmp.c
@@ -10,9 +10,12 @@ int ft_strlen(const char *s) {
   return (i);
 }
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int ft_memcmp(const *s1, const *s2, size_t n)
 {
     if (n == 0)
         return (0);
-    
+    while ((*s1 != '\0' || *s2 != '\0') && n > 0)
+    {
+      
+    }
 }
